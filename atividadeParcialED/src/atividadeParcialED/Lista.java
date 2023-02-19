7e974d6a7b20ec5553e1bda69e5467ef74088125
joaoAaf <joao.anderson.af@gmail.com>
2023-02-19 12:00:20 -0300

    Criação das classes Lista e No.

---
+++ atividadeParcialED/src/atividadeParcialED/Lista.java
@@ -0,0 +1,26 @@
+package atividadeParcialED;
+
+public class Lista {
+
+	private No primeiro;
+	private No ultimo;
+
+	/*public No getInicio() {
+		return primeiro;
+	}
+
+	public void setInicio(No inicio) {
+		this.primeiro = inicio;
+	}
+
+	public No getFim() {
+		return ultimo;
+	}
+
+	public void setFim(No fim) {
+		this.ultimo = fim;
+	}*/
+	
+	
+
+}
---
+++ atividadeParcialED/src/atividadeParcialED/No.java
@@ -0,0 +1,45 @@
+package atividadeParcialED;
+
+public class No {
+
+	private int dado;
+	//private No anterior;
+	private No proximo;
+
+	
+	
+	public No(int dado) {
+		this.dado = dado;
+		this.proximo = null;
+	}
+	
+	public No(int dado, No proximo) {
+		this.dado = dado;
+		this.proximo = proximo;
+	}
+
+	public int getDado() {
+		return dado;
+	}
+
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+
+	/*public No getAnterior() {
+		return anterior;
+	}
+
+	public void setAnterior(No anterior) {
+		this.anterior = anterior;
+	}*/
+
+	public No getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+
+}
